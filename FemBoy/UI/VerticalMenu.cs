131e8625b24cb0940a141742e9655eea81e9abd2
Stencho <c3142113@gmail.com>
2026-09-08 18:50:45 +1000

    Add slider to VerticalMenuSlider

--- FemBoy/UI/VerticalMenu.cs
+++ FemBoy/UI/VerticalMenu.cs
@@ -14,6 +14,9 @@ public class VerticalMenuItem {
 
     public virtual int item_height { get; set; } = 30;
 
+    public virtual void pressed_left() {}
+    public virtual void pressed_right() {}
+    
     public VerticalMenuItem(string text, Action on_pressed) {
         this.text = text;
         Pressed = on_pressed;
@@ -34,12 +37,60 @@ public class VerticalMenuItem {
 public class VerticalMenuSlider : VerticalMenuItem {
     public override int item_height { get; set; } = 60;
 
-    private int Value { get; set; } = 0; 
-    private int Minimum { get; set; } = 0; 
-    private int Maximum { get; set; } = 100; 
+    public int Value => (int)(Minimum + ((Maximum - Minimum) * value_f));
+    private float value_f = 0f;
+    public int Minimum { get; set; } = 0; 
+    public int Maximum { get; set; } = 100;
+
+    public int Step { get; set; } = 1;
+
+    public int Range => Maximum - Minimum;
     
-    public VerticalMenuSlider(string text, int min, int max, int value) : base(text, null) {
+    public VerticalMenuSlider(string text, int min, int max, int value, int step) : base(text, null) {
+        Minimum = min;
+        Maximum = max;
+        Step = step;
         
+        if (value < min) value_f = 0f;
+        else if (value > max) value_f = 1f;
+        else {
+            float position = (value - Minimum) / (float)Range;
+            value_f = position;
+        }
+    }
+
+    public override void draw(VerticalMenu parent, bool selected, Vector2i top_left, Vector2i bottom_right) {
+        var height_half = item_height / 2f;
+        var middle = top_left + (new Vector2(parent.menu_width, height_half + parent.item_gap) / 2f);
+        var middle_of_bottom = top_left + (new Vector2(parent.menu_width / 2f, height_half + (height_half / 2f)));
+            
+        Color fg = !selected ? UIColors.Foreground : UIColors.Background;
+        Color bg = selected ? UIColors.Foreground : UIColors.Background;
+        
+        if (selected) Draw2D.fill_rect(top_left, top_left + new Vector2i(parent.menu_width, item_height + parent.item_gap), UIColors.Foreground);
+        Draw2D.text_centered(parent.font, Text, middle, fg); 
+        
+        Draw2D.line_rounded_ends(
+            middle_of_bottom - (Vector2i.UnitX * (parent.menu_width / 3)),  
+            middle_of_bottom + (Vector2i.UnitX * (parent.menu_width / 3)), 
+            fg, 16f 
+            );
+
+        float max_dot_pos = (parent.menu_width / 3) * 2f;
+        
+        Draw2D.fill_circle(middle_of_bottom - (Vector2i.UnitX * (parent.menu_width / 3)) + (Vector2i.UnitX * (max_dot_pos * value_f)), 5f, bg);
+    }
+
+    public override void pressed_left() {
+        float s = Step / (float)Range;
+        value_f -= s;
+        if (value_f < 0f) value_f = 0f;
+    }
+
+    public override void pressed_right() {
+        float s = Step / (float)Range;
+        value_f += s;
+        if (value_f > 1f) value_f = 1f;
     }
 }
 
@@ -49,7 +100,7 @@ public class VerticalMenu : IMenuPage {
     VerticalMenuItem[] menu_items;
     
     public int menu_width = 300;
-    public int item_gap = 0;
+    public int item_gap = 4;
     public int selected_menu_item = 0;
 
     public string opened_by { get; set; } = "";
@@ -63,7 +114,7 @@ public class VerticalMenu : IMenuPage {
         this.menu_items = menu_items;
         
         foreach (var menu_item in menu_items) {
-            menu_height += menu_item.item_height;
+            menu_height += menu_item.item_height + item_gap;
         }
     }
     
@@ -90,8 +141,13 @@ public class VerticalMenu : IMenuPage {
         if (selected_menu_item > menu_items.Length - 1) selected_menu_item = 0;
     }
 
-    public void move_selection_left() { }
-    public void move_selection_right() { }
+    public void move_selection_left() {
+        menu_items[selected_menu_item].pressed_left();
+    }
+
+    public void move_selection_right() {
+        menu_items[selected_menu_item].pressed_right();
+    }
 
     public void confirm_selection() {
         menu_items[selected_menu_item].Pressed?.Invoke();
